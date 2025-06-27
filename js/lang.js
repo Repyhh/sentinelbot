4951e3348561e2f2879f7d411400683e43a43fc5
Repyhh <hugo.albinopereira@gmail.com>
2025-06-27 03:28:26 +0100

    bug

--- js/lang.js
+++ js/lang.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Carregar traduções do JSON
   const loadLanguage = (lang) => {
-    fetch(`../lang/${lang}.json`)
+    fetch(new URL(`../lang/${lang}.json`, import.meta.url))
       .then((response) => response.json())
       .then((translations) => applyTranslations(translations))
       .catch((error) => console.error("Erro ao carregar traduções:", error));
